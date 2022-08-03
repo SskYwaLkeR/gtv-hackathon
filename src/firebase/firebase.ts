5eb489b6a10d538ef06b8cbb78a9ff104dc9f823
Hrishi <hkrai39@gmail.com>
2022-08-03 11:48:52 +0530

    add firebase

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCfz4rbca2xdQTkj0eFKxd7GmCp70FzWhE",
+    authDomain: "gtv-tvolution.firebaseapp.com",
+    projectId: "gtv-tvolution",
+    storageBucket: "gtv-tvolution.appspot.com",
+    messagingSenderId: "291450058043",
+    appId: "1:291450058043:web:8683b11f7a41c90e4cb31e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
