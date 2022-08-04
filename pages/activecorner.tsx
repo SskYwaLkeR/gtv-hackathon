5d9b6357deeb2bb0842da865296fa45dda060af5
Arsh Kumar <arshkr07@gmail.com>
2022-08-04 18:17:15 +0530

    moved constants

--- pages/activecorner.tsx
+++ pages/activecorner.tsx
@@ -3,23 +3,24 @@ import { Header } from '../src/Components/Header';
 import { Moments } from '../src/Components/Moments';
 import { MomentsCategories } from '../src/Components/Moments/MomentCategories';
 
+const thriller = [
+	{
+		src: '/witcher.png',
+		timestamp: 'Today',
+	},
+	{
+		src: '/witcher_logo.png',
+		timestamp: '2 days ago',
+	},
+];
+const emotional = [
+	{
+		src: '/witcher_person.png',
+		timestamp: 'Today',
+	},
+];
+
 const Activecorner = () => {
-	const thriller = [
-		{
-			src: '/witcher.png',
-			timestamp: 'Today',
-		},
-		{
-			src: '/witcher_logo.png',
-			timestamp: '2 days ago',
-		},
-	];
-	const emotional = [
-		{
-			src: '/witcher_person.png',
-			timestamp: 'Today',
-		},
-	];
 	return (
 		<div
 			style={{
