83290bd61001ceabd22693bd316af39af77d5b4f
Hrishi <hkrai39@gmail.com>
2022-08-04 11:33:01 +0530

    add footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from "next";
 import { ShoppingPage } from "../src/Component/ShoppingPage";
+import { Footer } from "../src/Component/Footer";
 
 const Home: NextPage = () => {
   return (
     <div>
       <ShoppingPage />
+      <Footer />
     </div>
   );
 };
